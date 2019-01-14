6d776f3a678e576e0c1656c08874be0d52ee0bfd
Greg Chattin-McNichols <gchattinmcnichols@pivotal.io>
2019-01-14 10:17:09 -0800

    A pyrrhic victory over DB test pollution

--- src/test/kotlin/com/binaryLoyalty/server/IntegrationTests/IntegrationTestsBaseWD.kt
+++ src/test/kotlin/com/binaryLoyalty/server/IntegrationTests/IntegrationTestsBaseWD.kt
@@ -1,41 +1,54 @@
 package com.binaryLoyalty.server.IntegrationTests
 
-import com.binaryLoyalty.server.GameRepository
-import com.nhaarman.expect.expect
 import org.junit.After
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.firefox.FirefoxDriver
 import org.reflections.Reflections
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.test.web.client.getForEntity
-import org.springframework.boot.test.web.client.postForEntity
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.context.ApplicationContext
 import org.springframework.data.repository.CrudRepository
-import org.springframework.http.*
 import org.springframework.test.context.junit4.SpringRunner
-import org.springframework.util.MultiValueMap
+import java.lang.reflect.InvocationTargetException
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IntegrationTestsBaseWD {
 
-    val driver : WebDriver = ChromeDriver()
+    @Autowired
+    private lateinit var appContext: ApplicationContext
+
+    val driver: WebDriver = ChromeDriver()
 
     @LocalServerPort
     var port: Int? = null
 
-    fun baseUrl() : String = "http://localhost:$port"
+    fun baseUrl(): String = "http://localhost:$port"
 
     @After
     fun tearDown() {
-        driver.quit()
+        try {
+            val reflections = Reflections("com.binaryLoyalty.server")
+            val repos = reflections.getSubTypesOf(CrudRepository::class.java)
+            val secondRoundRepos = mutableSetOf<Any>()
+            for (repo in repos) {
+                try {
+                    repo.getMethod("deleteAll").invoke(appContext.getBean(repo.simpleName.decapitalize()))
+                } catch (e: InvocationTargetException) {
+                    secondRoundRepos.add(repo)
+                }
+            }
+            for (repo in secondRoundRepos) {
+                if (repo is Class<*>) {
+                    repo.getMethod("deleteAll").invoke(appContext.getBean(repo.simpleName.decapitalize()))
+                }
+            }
+        } finally {
+            driver.quit()
+        }
     }
 
     @Test
